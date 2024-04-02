169e964fc44b74e057eae2fb9ec5cb1039b10e0b
aporosh <115151056+aporosh@users.noreply.github.com>
2024-04-02 12:18:53 +0300

    update RepoGUI, add favorites functions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLazyGetUserReposQuery, useSearchUsersQuery } from "../store/github/github.api";
-import { useDebounce } from "./hooks/debounce";
+import { useDebounce } from "../hooks/debounce";
+import { RepoCard } from "../components/RepoCard";
 
 
 export function HomePage() {
@@ -19,6 +20,7 @@ export function HomePage() {
 
     const clickHandler = (username: string) => {
         fetchRepos(username)
+        setDropdown(false)
     }
 
     return (
@@ -44,9 +46,10 @@ export function HomePage() {
                         >{user.login}</li>
                     ))}
                 </ul>}
-                
+
                 <div className="container ">
                     {areReposLoading && <p className="text-center">Repos are loading...</p>}
+                    { repos?.map(repo => <RepoCard repo={repo} key={repo.id} />) }
                 </div>
             </div>
 
